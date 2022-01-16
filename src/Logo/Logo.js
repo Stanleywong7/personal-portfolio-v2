37dca1a771e93f8fcbdd44f15ed9b73b05a1ffe2
Stanley Wong <31183863+Stanleywong7@users.noreply.github.com>
2022-01-16 17:34:41 -0500

    Added Framer Motion and Logo Animation

--- src/Logo/Logo.js
+++ src/Logo/Logo.js
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import { motion } from "framer-motion";
 import logo from "./full_logo.svg";
 import "./Logo.css";
 
 function Logo() {
+  const [isActive, setIsActive] = React.useState(false);
+
   return (
     <div>
-      <img src={logo} className="logo" alt="logo" />
+      <motion.img 
+        src={logo}
+        className={isActive ? `rotate` : null}
+        id="logo"
+        alt="logo"
+        onClick={() => setIsActive(!isActive)}
+        animate={{
+          rotate: isActive ? 180 : 0,
+        }}
+      />
     </div>
   );
-}
+};
 
 export default Logo;
