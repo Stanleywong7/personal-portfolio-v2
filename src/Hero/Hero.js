33b0a80e5125eccd6ea61a46da7aa6ab5f5df872
Stanley Wong <31183863+Stanleywong7@users.noreply.github.com>
2022-01-15 22:08:09 -0500

    Hero content update

--- src/Hero/Hero.js
+++ src/Hero/Hero.js
@@ -4,7 +4,15 @@ import "./Hero.css";
 function Hero() {
   return (
     <div>
-      <p>Welcome to my portfolio!</p>
+      <div className="welcome-text">
+        <h2>Welcome!</h2>
+        <p>
+          The site is still in development. <br />
+          Please feel free to check out my socials below.
+        </p>
+      </div>
+
+      <div className="social-icons"></div>
     </div>
   );
 }
