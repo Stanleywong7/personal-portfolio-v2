9e65e0048fb3c5e96c713f91d644f6d2cdeb985a
Stanley Wong <31183863+Stanleywong7@users.noreply.github.com>
2022-01-15 22:25:12 -0500

    Removed index.html

--- src/Hero/Hero.js
+++ src/Hero/Hero.js
@@ -7,12 +7,13 @@ function Hero() {
       <div className="welcome-text">
         <h2>Welcome!</h2>
         <p>
-          The site is still in development. <br />
+          The website is still in development. <br />
           Please feel free to check out my socials below.
         </p>
       </div>
 
       <div className="social-icons"></div>
+      <i class="fab fa-github"></i>
     </div>
   );
 }
