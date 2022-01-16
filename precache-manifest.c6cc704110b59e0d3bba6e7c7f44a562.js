c1c6002e26fcfea24a21f8fe9e8833da0b2e5678
Stanley Wong <31183863+Stanleywong7@users.noreply.github.com>
2022-01-15 22:07:56 -0500

    Updates

---
+++ precache-manifest.c6cc704110b59e0d3bba6e7c7f44a562.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "677bf4db8dcce2b5d32c6c7ccf21c862",
+    "url": "/Stanleywong7/personal-portfolio-v2/index.html"
+  },
+  {
+    "revision": "ed8d70e6bdc3e850d41f",
+    "url": "/Stanleywong7/personal-portfolio-v2/static/css/main.d9396445.chunk.css"
+  },
+  {
+    "revision": "5caeaae02598237feb23",
+    "url": "/Stanleywong7/personal-portfolio-v2/static/js/2.ea490775.chunk.js"
+  },
+  {
+    "revision": "10b61eb3fc61dfd2241d",
+    "url": "/Stanleywong7/personal-portfolio-v2/static/js/3.80c7ac3e.chunk.js"
+  },
+  {
+    "revision": "ed8d70e6bdc3e850d41f",
+    "url": "/Stanleywong7/personal-portfolio-v2/static/js/main.f0b15908.chunk.js"
+  },
+  {
+    "revision": "0dd06f68970093aa8ec2",
+    "url": "/Stanleywong7/personal-portfolio-v2/static/js/runtime~main.a1f8e6bc.js"
+  },
+  {
+    "revision": "e8a9b070b41af46c5f611a287de310e1",
+    "url": "/Stanleywong7/personal-portfolio-v2/static/media/full_logo.e8a9b070.svg"
+  }
+]);
